d4b2addfb05d022ceb932dbde4a2dc8de3981f9c
Mohara88s <mohara88s@gmail.com>
2021-10-18 13:20:47 +0300

    hw3 fix schema & port

--- models/contact.js
+++ models/contact.js
@@ -1,19 +1,24 @@
 const { Schema, model } = require('mongoose')
 const Joi = require('joi')
 
-const nameRegex = /^[a-zA-Z ]{3,30}$/
+const nameRegex = /^[a-zA-Z ]{3,35}$/
 const phoneRegex = /^[0-9()-]{10,25}$/
+const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 const contactSchema = Schema({
   name: {
     type: String,
     required: [true, 'Set name for contact'],
     match: nameRegex,
+    unique: true,
   },
   email: {
     type: String,
+    required: [true, 'Set email for contact'],
+    match: emailRegex,
   },
   phone: {
     type: String,
+    required: [true, 'Set phone for contact'],
     minlength: 10,
     match: phoneRegex,
   },
